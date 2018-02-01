c366817d330d6a3f9eb2693974674e0dda2e55e7
yangglive <yangglive@icloud.com>
2018-02-01 09:40:50 +0800

    add Variables

--- console/components/logs/index.js
+++ console/components/logs/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {getLogs,cancelLogs} from "../../reducers/logs"
+import {cancelLogs, getLogs} from "../../reducers/logs"
 import Close from '../icons/close'
 import SwapHoriz from '../icons/swapHoriz'
 import Refresh from '../icons/refresh'
@@ -24,29 +24,57 @@ const mapDispatchToProps = (dispatch) => {
     }
 };
 
+let isIe = false;
+
+if (/MSIE/i.test(navigator.userAgent)) {
+    isIe = true;
+}
+if (/rv:11.0/i.test(navigator.userAgent)) {
+    isIe = true;
+}
+if (/Edge\/\d./i.test(navigator.userAgent)) {
+    isIe = true;
+}
+
 class Logs extends React.Component {
     constructor(props) {
         super(props);
+
     }
 
     componentWillMount() {
+        if (isIe) {
+            return
+        }
         this.props.getLogs();
     }
 
     componentDidMount() {
         this.el = document.getElementById("logs");
-        this.handleScroll();
+        if (isIe) {
+
+        } else {
+            this.handleScroll();
+        }
     }
 
     componentWillReceiveProps(nextProps) {
+        if (isIe) {
+            return
+        }
         this.handleScroll();
     }
 
     handleRefresh = () => {
+        if (isIe) {
+            return
+        }
         this.props.getLogs();
     };
-
     handleScroll = () => {
+        if (isIe) {
+            return
+        }
         if (this.el) {
             let scrolltop = this.el.scrollTop;
             let clientHeight = this.el.clientHeight;
@@ -61,24 +89,31 @@ class Logs extends React.Component {
     };
 
     handleClose = () => {
-        this.props.cancelLogs();
+        if (isIe) {
+
+        } else {
+            this.props.cancelLogs();
+        }
         if (this.props.onClose) {
             this.props.onClose()
         }
     };
 
     renderList() {
-        const {logs} = this.props;
-
-        return <ul id="logs">
-            {logs.list.map((log, index) => {
-                return <li className="item" key={index}>
+        if (isIe) {
+            return <iframe className="iframe-logs" src="/api/logs" id="logs"/>
+        } else {
+            const {logs} = this.props;
+            return <ul id="logs">
+                {logs.list.map((log, index) => {
+                    return <li className="item" key={index}>
                     <pre>
                         {log}
                     </pre>
-                </li>
-            })}
-        </ul>
+                    </li>
+                })}
+            </ul>
+        }
     }
 
     render() {
@@ -89,11 +124,17 @@ class Logs extends React.Component {
             network += " error";
             error = logs.fetchLogs.error;
         }
+        let renderIe = "";
+        if (isIe) {
+            renderIe = "This is an iframe under IE or Edge";
+        }
         return <div className="logs">
             <div className="logs-toolbar">
                 <SwapHoriz className={network}/>
                 {error === "" ? null :
                     <div className="error">{error}<Refresh className="refresh" onClick={this.handleRefresh}/></div>}
+                {renderIe === "" ? null :
+                    <div className="error ie">{renderIe}</div>}
                 <Close className="close" onClick={this.handleClose}/>
             </div>
             {this.renderList()}
