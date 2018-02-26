adf0708c486d4c3f908529210e33453ca6b464c0
yangglive <yangglive@icloud.com>
2018-02-26 11:29:59 +0800

    todo

--- doc.go
+++ doc.go
@@ -1,8 +1,9 @@
 // riff is a tool for discovering and configuring services in your infrastructure
 package riff
 
-/*todo 1，增加服务版本号
-2，增加服务log tail -f查看
-3，支持服务手动升级。
-4，支付服务注册。服务网络监控
+/*
+todo 1，增加服务版本号
+todo 2，增加服务log tail -f查看
+todo 3，支持服务手动升级。
+todo 4，支付服务注册。服务网络监控
  */
