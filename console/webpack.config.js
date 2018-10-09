b389ded912ed29b4a1cd341e1b0942c53b93704b
yangglive <yangglive@icloud.com>
2018-10-09 22:15:33 +0800

    update webpack

--- console/webpack.config.js
+++ console/webpack.config.js
@@ -1,12 +1,12 @@
 /**
  * Created by yanggang on 2017/3/6.
  */
-var path = require("path");
-var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var autoprefixer = require('autoprefixer');
-var precss = require('precss');
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 const env = process.env.NODE_ENV || 'development';
 
@@ -19,101 +19,123 @@ module.exports = {
         filename: "[name].js"
     },
     externals: {
-        'config': function(){
+        config: function () {
             return JSON.stringify(require('./config/' + env + '.json'));
         }()
     },
-    plugins: function(){
-        var options = [
-            new HtmlWebpackPlugin({title:'Riff Console',hash:true,inject:false, filename: 'console.html', template: 'index.html'}),
-            new ExtractTextPlugin({filename: "[name].css", disable: false, allChunks: true}),
-            new webpack.DefinePlugin({'process.env': {NODE_ENV: JSON.stringify(env)}}),
-        ];
-        if(env === 'production' || env === 'testing') {
-            options.push(new webpack.optimize.UglifyJsPlugin());
-            options.push(new webpack.LoaderOptionsPlugin({minimize: true}));
-        }
-        options.push(new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: "jquery"
-        }));
-        return options;
-    }(),
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            automaticNameDelimiter: '-',
+            minSize: 2000,
+            cacheGroups: {
+                react: {
+                    test: (module, chunks) => /react/.test(module.context),
+                    minChunks: 1,
+                    name: "react",
+                    priority: -10,
+                },
+                antd: {
+                    test: (module, chunks) => /antd|rc-|async-validator/.test(module.context),
+                    minChunks: 1,
+                    name: "antd",
+                    priority: -20,
+                },
+                vendors: {
+                    test: /node_modules/,
+                    minChunks: 1,
+                    name: "vendors",
+                    priority: -30,
+                },
+                default: {
+                    minChunks: 1,
+                    priority: -40,
+                    chunks: 'all'
+                }
+            }
+        },
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true // set to true if you want JS source maps
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Riff Console",
+            hash: true,
+            inject: false,
+            filename: "console.html",
+            template: "index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            disable: false,
+            allChunks: true
+        }),
+        new webpack.DefinePlugin({
+            "process.env": {NODE_ENV: JSON.stringify(env)}
+        })
+    ],
     module: {
         rules: [{
+            test: /\.flow/, loader: 'ignore-loader'
+        }, {
             test: /\.js?$/,
             exclude: /node_modules/,
             use: {
-                loader: 'babel-loader',
+                loader: "babel-loader",
                 options: {
-                    presets: ['env', 'react', 'stage-0'],
-                    plugins: [["import", {
-                        "libraryName": "antd",
-                        "style": true
-                    }]]
+                    presets: ["env", "react", "stage-0"],
+                    plugins: [
+                        [
+                            "import",
+                            {
+                                libraryName: "antd",
+                                style: true
+                            }
+                        ]
+                    ]
                 }
             }
-        },{
+        }, {
             test: /.css$/,
-            use: ExtractTextPlugin.extract({
-                fallback: "style-loader",
-                use: [
-                    "css-loader",
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: function () {
-                                return [precss,autoprefixer]
-                            }
-                        }
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader",
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        ident: 'postcss',
+                        plugins: (loader) => [
+                            require('postcss-import')({root: loader.resourcePath}),
+                            require('postcss-cssnext')()
+                        ]
                     }
-                ]
-            })
-        },{
+                }
+            ]
+        }, {
             test: /.less$/,
-            use: ExtractTextPlugin.extract({
-                fallback: "style-loader",
-                use: [
-                    "css-loader",
-                    {
-                        loader: 'postcss-loader', options: {
-                            plugins: function () {
-                                return [precss,autoprefixer]
-                            }
-                        }
-                    },
-                    {
-                        loader: 'less-loader', options: {
-                            //modifyVars: themes()
-                        }
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader",
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        ident: 'postcss',
+                        plugins: (loader) => [
+                            require('postcss-import')({root: loader.resourcePath}),
+                            require('postcss-cssnext')()
+                        ]
                     }
-                ]
-            })
-        },{
-            test: require.resolve('jquery'),
-            loader: "imports-loader?$=jquery&jQuery=jquery"
-        },{
-            test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'file-loader',
-            options: {
-                name: 'fonts/[name].[ext]',
-            }
-        },{
-            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader',
-            options: {
-                name: 'fonts/[name].[ext]',
-            }
-        },{
-            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader',
-            options: {
-                name: 'fonts/[name].[ext]',
-            }
-        },{
-            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader',
-            options: {
-                name: 'fonts/[name].[ext]',
-            }
-        }
-        ]
-    },
+                },
+                {
+                    loader: "less-loader"
+                }
+            ]
+        }]
+    }
 };
