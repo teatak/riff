3278936ca5994b8bdf2655ec0335a9a23af19bef
yangglive <yangglive@icloud.com>
2018-10-10 11:34:18 +0800

    update webpack

--- riff/server.go
+++ riff/server.go
@@ -159,8 +159,7 @@ func (s *Server) setupCart() error {
 		r.Use("/console/*file", cart.File("../static/dist/console.html"))
 		r.Use("/static/*file", cart.Static("../static", false))
 	}
-	sc := graphql.MustParseSchema(schema.String(), &resolver.Resolver{})
-	h := &Http{Schema:sc}
+	h := &Http{Schema:graphql.MustParseSchema(schema.String(), &resolver.Resolver{})}
 	r.Route("/", h.Index)
 	s.httpServer = r.ServerKeepAlive(s.config.Addresses.Http + ":" + strconv.Itoa(s.config.Ports.Http))
 	return nil
