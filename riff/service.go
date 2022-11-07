79b42963ca3a76da98fb9ee7784ffe9a9ed30075
Yang <yangglive@icloud.com>
2022-11-07 16:43:19 +0800

    add log

--- riff/service.go
+++ riff/service.go
@@ -416,7 +416,7 @@ func (s *Service) processGit(client git.Client) {
 		version, asset, err = client.GetRelease(version)
 	}
 	if err != nil {
-		server.Logger.Printf(errorServicePrefix+"%s find version error: %v", s.Name, err)
+		server.Logger.Printf(errorServicePrefix+"%s type:%s version:%s sha:%s asset:%s find version error: %v", s.Name, t, v, version, asset, err)
 		return
 	}
 	//check local version
